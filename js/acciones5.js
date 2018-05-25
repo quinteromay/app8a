0107ac424445c56b02eb3fd9a432315d8747113b
aryit <38222680+aryit@users.noreply.github.com>
2018-05-25 07:40:51 +0200

    subida30
    app8a

--- js/acciones5.js
+++ js/acciones5.js
@@ -1,12 +1,11 @@
 // JavaScript Document
-$(document).ready(function(e){
-    document.addEventListener("deviceready", function(){
-		$('#sonar').tap(function(){
+$ (document).ready(function (e) {
+	document.addEventListener("deviceready", function() {
+		$('#beep').tap(function() {
 			navigator.notification.beep(2);
-		}); //ciere del beep
-		$('#vibrar').tap(function(){
+		}); //cierre del beep
+		$('#vibrar').tap(function() {
 			navigator.notification.vibrate(2000);
-		}); //cierre del vibrar
+		});//cierre del vibrar
 	},false); //cierre del deviceready
-	
-}); //cierre document o ready
+}); // cierre ready
